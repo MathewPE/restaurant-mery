2f01106087ab4ced99587157a07f558ade273c64
Nataly Rojas <natalyrojasmoreno@Nataly-Rojas-2.local>
2023-09-09 13:56:11 -0500

    feat: structure initial

---
+++ src/components/GridCardProducts/index.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div>
+    <p>Grid Card CardProduct</p>
+    <CardProduct />
+  </div>
+</template>
+
+<script setup>
+ import CardProduct from '@components/CardProduct/index.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
